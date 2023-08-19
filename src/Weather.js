629f252007379f8139eb31734a04d234bb772027
nolans42 <114963614+nolans42@users.noreply.github.com>
2023-08-19 15:24:51 +1000

    installed axios

---
+++ src/Weather.js
@@ -0,0 +1,78 @@
+import React from "react";
+import axios from "axios";
+import "./search.css";
+
+export default function Weather() {
+  let appData = {
+    city: "New York",
+    temperature: 12,
+    date: "Friday 18:00",
+    description: "Partly Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    humidity: 73,
+    wind: 2,
+    precipitaion: 5,
+  };
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="weather-app-wrapper">
+          <div className="weather-app">
+            <div className="overview">
+              <div className="Weather">
+                <form className="mb-3">
+                  <div className="row">
+                    <div className="col-9">
+                      <input
+                        type="search"
+                        placeholder="Type a city.."
+                        className="form-control"
+                        autoComplete="off"
+                      />
+                      <input
+                        type="submit"
+                        value="Search"
+                        className="btn btn-primary w-100"
+                      />
+                    </div>
+                  </div>
+                </form>
+                <h1>{appData.city}</h1>
+                <ul>
+                  <li>{appData.date}</li>
+                  <li>{appData.description}</li>
+                </ul>
+              </div>
+              <div className="row">
+                <div className="col-6">
+                  <div className="d-flex weather-temperature">
+                    <img
+                      src={appData.imgUrl}
+                      alt={appData.description}
+                      id="icon"
+                    />
+                    <strong>{appData.temperature}</strong>
+                    <span className="units">ºC| ºF</span>
+                  </div>
+                </div>
+                <div className="col-6">
+                  <ul>
+                    <li>Precipitaion: {appData.precipitaion}%</li>
+                    <li>Humidity: {appData.humidity}%</li>
+                    <li>Wind: {appData.wind} km/h</li>
+                  </ul>
+                </div>
+              </div>
+              <small>
+                <a href="https://github.com/nolans42/Vanilla-App-Weather">
+                  Open-source code
+                </a>
+                by Sarah Nolan
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
