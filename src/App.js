a7a367105e02e12bc9d27ce0cdbc0df25c89139c
nolans42 <114963614+nolans42@users.noreply.github.com>
2023-08-21 16:05:30 +1000

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import "./search.css";
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <Weather />
       </header>
+    </div>
     </div>
   );
 }
