424a630ce59f296826883221c7e5e0655fc1b642
nolans42 <114963614+nolans42@users.noreply.github.com>
2023-09-12 14:33:43 +1000

    updated footer to include netlify

--- src/App.js
+++ src/App.js
@@ -14,6 +14,14 @@ function App() {
             This project is coded by Sarah Nolan and is {""}
             <a href="https://github.com/nolans42/weather-react">
               Open-sourced on Github {""}
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://master--eclectic-pudding-7c6c06.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify
             </a>
           </small>
         </footer>
